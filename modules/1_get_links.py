84097d6eca3aa3e78304487cc29e9005b653c43d
Olena Kysilova <e.a.kiseljova@gmail.com>
2026-02-11 21:26:09 +0200

    Complete request / BS4 task

---
+++ modules/1_get_links.py
@@ -0,0 +1,11 @@
+from load_django import *  # noqa
+
+from parser_app.models import ProductInfo, Status
+
+# In a real situation, there will be an infinite while True loop here, as long as the page request returns a 200 status code
+data = {
+    "link": "https://brain.com.ua/ukr/Mobilniy_telefon_Apple_iPhone_16_Pro_Max_256GB_Black_Titanium-p1145443.html",
+    "status": Status.NEW,
+}
+
+ProductInfo.objects.get_or_create(**data)
--- modules/2_get_info.py
+++ modules/2_get_info.py
@@ -15,11 +15,20 @@ Characteristics
 ***************
 """
 
-import pprint
+import pprint  # noqa
+import random
+import time
+from concurrent.futures import ThreadPoolExecutor
 
 from bs4 import BeautifulSoup
 from requests import Session
 from requests.exceptions import RequestException
+
+from load_django import *  # noqa
+from parser_app.models import ProductInfo, Status
+
+# Max threads
+THREADS = 5
 
 # 1. Initialize the Session
 session = Session()
@@ -46,191 +55,212 @@ initial_cookies = {
 session.cookies.update(initial_cookies)
 
 
-def get_product_data(url: str) -> dict | None:
+def get_product_data(parsed_data: ProductInfo) -> dict | None:
     """
     Parse product data from https://brain.com.ua
-    :param url:
+    :param parsed_data:
     :return:
     """
     try:
         # We don't need to pass headers/cookies here; they are held by the session
-        response = session.get(url)
+        response = session.get(parsed_data.link)
         response.raise_for_status()  # Raises an error for 4xx or 5xx codes
+    except RequestException as e:
+        print(f"❌ Error fetching {parsed_data.link}: {e}")
 
-        soup = BeautifulSoup(response.text, "html.parser")
+        return None
 
-        # Product Data
-        product_info = {}
+    soup = BeautifulSoup(response.text, "html.parser")
 
-        # Name
+    # Product Data
+    product_info = {}
+
+    # Name
+    try:
+        product_info["name"] = soup.select_one(
+            "h1.main-title"
+        ).text.strip()  # //h1[@class='main-title']
+    except AttributeError as e:
+        print("❌ Error Name", e)
+        product_info["name"] = None
+        # TO DO: logging
+
+    # Color
+    try:
+        product_info["color"] = soup.select_one(
+            "a[title^='Колір']"
+        ).text.strip()  # //a[starts-with(@title, 'Колір')]
+    except AttributeError as e:
+        print("❌ Error Color", e)
+        product_info["color"] = None
+        # TO DO: logging
+
+    # Built-in Memory
+    try:
+        product_info["builtin_memory"] = soup.select_one(
+            "a[title^='Вбудована пам']"
+        ).text.strip()  # //a[starts-with(@title, 'Вбудована пам')]
+    except AttributeError as e:
+        print("❌ Error Built-in Memory", e)
+        product_info["builtin_memory"] = None
+        # TO DO: logging
+
+    # Manufacturer
+    try:
+        product_info["manufacturer"] = (
+            soup.find(string="Виробник").find_next("span").text.strip()
+        )
+        # //span[text()='Виробник']/following-sibling::span
+    except AttributeError as e:
+        print("❌ Error Manufacturer", e)
+        product_info["manufacturer"] = None
+        # TO DO: logging
+
+    # Prices
+    try:
+        prices = soup.select(
+            ".main-price-block .price-wrapper > span"
+        )  # //*[contains(@class, 'main-price-block')]//*[@class='price-wrapper']/span
+    except AttributeError as e:
+        print("❌ Error Prices", e)
+        product_info["price_regular"] = None
+        product_info["price_sale"] = None
+        # TO DO: logging
+    else:
+        # Regular Price
         try:
-            product_info["name"] = soup.select_one(
-                "h1.main-title"
-            ).text.strip()  # //h1[@class='main-title']
-        except AttributeError as e:
-            print("❌ Error Name", e)
-            product_info["name"] = None
-            # TO DO: logging
-
-        # Color
-        try:
-            product_info["color"] = soup.select_one(
-                "a[title^='Колір']"
-            ).text.strip()  # //a[starts-with(@title, 'Колір')]
-        except AttributeError as e:
-            print("❌ Error Color", e)
-            product_info["color"] = None
-            # TO DO: logging
-
-        # Built-in Memory
-        try:
-            product_info["builtin_memory"] = soup.select_one(
-                "a[title^='Вбудована пам']"
-            ).text.strip()  # //a[starts-with(@title, 'Вбудована пам')]
-        except AttributeError as e:
-            print("❌ Error Built-in Memory", e)
-            product_info["builtin_memory"] = None
-            # TO DO: logging
-
-        # Manufacturer
-        try:
-            product_info["manufacturer"] = (
-                soup.find(string="Виробник").find_next("span").text.strip()
-            )
-            # //span[text()='Виробник']/following-sibling::span
-        except AttributeError as e:
-            print("❌ Error Manufacturer", e)
-            product_info["manufacturer"] = None
-            # TO DO: logging
-
-        # Prices
-        try:
-            prices = soup.select(
-                ".main-price-block .price-wrapper > span"
-            )  # //*[contains(@class, 'main-price-block')]//*[@class='price-wrapper']/span
-        except AttributeError as e:
-            print("❌ Error Prices", e)
+            product_info["price_regular"] = prices[0].text.strip()
+        except IndexError:
             product_info["price_regular"] = None
+            # TO DO: logging
+
+        # Sale Price
+        try:
+            product_info["price_sale"] = prices[1].text.strip()
+        except IndexError:
             product_info["price_sale"] = None
             # TO DO: logging
-        else:
-            # Regular Price
+
+    # SKU
+    try:
+        sku = soup.select_one(".br-pr-code-val")
+
+        product_info["sku"] = sku.text.strip()
+        # //*[@class='br-pr-code-val']
+    except AttributeError as e:
+        print("❌ Error SKU", e)
+        product_info["sku"] = None
+        # TO DO: logging
+
+    # Reviews Count
+    try:
+        product_info["reviews_count"] = soup.select_one(
+            ".reviews-count span"
+        ).text.strip()
+        # //*[contains(@class, 'reviews-count')]/span
+    except AttributeError as e:
+        print("❌ Error Review Count", e)
+        product_info["reviews_count"] = None
+        # TO DO: logging
+
+    # Images
+    try:
+        images = soup.select(".br-pr-slider .br-main-img")
+        # //*[contains(@class, 'br-pr-slider')]//img[@class='br-main-img']
+
+        images_srcs = []
+        for image in images:
+            images_srcs.append(image.get("src"))
+
+        product_info["images"] = images_srcs
+    except AttributeError as e:
+        print("❌ Error Images", e)
+        product_info["images"] = None
+        # TO DO: logging
+
+    # Characteristics
+    try:
+        characteristics = soup.select(".br-pr-chr-item")
+        # //*[@class='br-pr-chr-item']
+
+        characteristics_list = []
+        for characteristic in characteristics:
             try:
-                product_info["price_regular"] = prices[0].text.strip()
-            except IndexError:
-                product_info["price_regular"] = None
+                title = characteristic.select_one("h3").text.strip()
+
+                items = characteristic.select("div > div")
+            except AttributeError:
                 # TO DO: logging
+                continue
+            else:
+                items_dict = {}
+                for item in items:
+                    try:
+                        key, value, *rest = item.select("span")
+                    except AttributeError:
+                        # TO DO: logging
+                        continue
+                    else:
+                        # Default parameter
+                        param_name = None
+                        param_value = None
 
-            # Sale Price
-            try:
-                product_info["price_sale"] = prices[1].text.strip()
-            except IndexError:
-                product_info["price_sale"] = None
-                # TO DO: logging
-
-        # SKU
-        try:
-            sku = soup.select_one(".br-pr-code-val")
-
-            product_info["sku"] = sku.text.strip()
-            # //*[@class='br-pr-code-val']
-        except AttributeError as e:
-            print("❌ Error SKU", e)
-            product_info["sku"] = None
-            # TO DO: logging
-
-        # Reviews Count
-        try:
-            product_info["reviews_count"] = soup.select_one(
-                ".reviews-count span"
-            ).text.strip()
-            # //*[contains(@class, 'reviews-count')]/span
-        except AttributeError as e:
-            print("❌ Error Review Count", e)
-            product_info["reviews_count"] = None
-            # TO DO: logging
-
-        # Images
-        try:
-            images = soup.select(".br-pr-slider .br-main-img")
-            # //*[contains(@class, 'br-pr-slider')]//img[@class='br-main-img']
-
-            images_srcs = []
-            for image in images:
-                images_srcs.append(image.get("src"))
-
-            product_info["images"] = images_srcs
-        except AttributeError as e:
-            print("❌ Error Images", e)
-            product_info["images"] = None
-            # TO DO: logging
-
-        # Characteristics
-        try:
-            characteristics = soup.select(".br-pr-chr-item")
-            # //*[@class='br-pr-chr-item']
-
-            characteristics_list = []
-            for characteristic in characteristics:
-                try:
-                    title = characteristic.select_one("h3").text.strip()
-
-                    items = characteristic.select("div > div")
-                except AttributeError:
-                    # TO DO: logging
-                    continue
-                else:
-                    items_dict = {}
-                    for item in items:
                         try:
-                            key, value, *rest = item.select("span")
+                            param_name = key.text.strip()
+                            param_value = value.select_one("a").text.strip()
                         except AttributeError:
                             # TO DO: logging
                             continue
                         else:
-                            # Default parameter
-                            param_name = None
-                            param_value = None
+                            items_dict[param_name] = param_value
+                        finally:
+                            # Screen Diagonal
+                            if param_name == "Діагональ екрану":
+                                product_info["screen_diagonal"] = param_value
 
-                            try:
-                                param_name = key.text.strip()
-                                param_value = value.select_one("a").text.strip()
-                            except AttributeError:
-                                # TO DO: logging
-                                continue
-                            else:
-                                items_dict[param_name] = param_value
-                            finally:
-                                # Screen Diagonal
-                                if param_name == "Діагональ екрану":
-                                    product_info["screen_diagonal"] = param_value
+                            # Screen Resolution
+                            if param_name == "Роздільна здатність екрану":
+                                product_info["screen_resolution"] = param_value
 
-                                # Screen Resolution
-                                if param_name == "Роздільна здатність екрану":
-                                    product_info["screen_resolution"] = param_value
+                # Append characteristic list
+                characteristics_list.append((title, items_dict))
 
-                    # Append characteristic list
-                    characteristics_list.append((title, items_dict))
+        product_info["characteristics"] = characteristics_list
+    except AttributeError:
+        product_info["characteristics"] = None
+        # TO DO: logging
 
-            product_info["characteristics"] = characteristics_list
-        except AttributeError:
-            product_info["characteristics"] = None
-            # TO DO: logging
+    # Saving product_info into DB
+    for key, value in product_info.items():
+        setattr(parsed_data, key, value)
 
-        return product_info
+    parsed_data.status = Status.DONE
+    parsed_data.save()
 
-    except RequestException as e:
-        print(f"❌ Error fetching {url}: {e}")
+    # Timeout
+    time.sleep(random.uniform(1, 3))
 
-        return None
+    return product_info
+
+
+# if __name__ == "__main__":
+#     # URL
+#     target_url = "https://brain.com.ua/ukr/Mobilniy_telefon_Apple_iPhone_16_Pro_Max_256GB_Black_Titanium-p1145443.html"
+#
+#     # Get data
+#     data = get_product_data(target_url)
+#
+#     # Display data
+#     pprint.pprint(data)
+
+
+def main():
+    # Get New ProductInfo
+    parsed_links = ProductInfo.objects.filter(status=Status.NEW)
+
+    with ThreadPoolExecutor(max_workers=THREADS) as executor:
+        executor.map(get_product_data, parsed_links)
 
 
 if __name__ == "__main__":
-    # URL
-    target_url = "https://brain.com.ua/ukr/Mobilniy_telefon_Apple_iPhone_16_Pro_Max_256GB_Black_Titanium-p1145443.html"
-
-    # Get data
-    data = get_product_data(target_url)
-
-    # Display data
-    pprint.pprint(data)
+    main()
