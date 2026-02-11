290d617860716d205457189e9faf66e022795356
Olena Kysilova <e.a.kiseljova@gmail.com>
2026-02-11 20:23:51 +0200

    Add status to ProductInfo model

--- braincomua_project/parser_app/models.py
+++ braincomua_project/parser_app/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 
 class Status(models.TextChoices):
-    NEW = "NE", "New"
-    DONE = "DO", "Done"
-    FAILED = "FA", "Failed"
+    NEW = "NW", "New"
+    DONE = "DE", "Done"
+    FAILED = "FD", "Failed"
 
 
 class ProductInfo(models.Model):
@@ -20,5 +20,6 @@ class ProductInfo(models.Model):
     reviews_count = models.IntegerField(null=True, blank=True)
     images = ArrayField(models.CharField(max_length=255), null=True, blank=True)
     characteristics = models.JSONField(null=True, blank=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.NEW)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
