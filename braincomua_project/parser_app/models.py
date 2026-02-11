10f99245b621b330a07afb540c7468f6677872ec
Olena Kysilova <e.a.kiseljova@gmail.com>
2026-02-11 18:58:50 +0200

    Add ProductInfo model

--- braincomua_project/parser_app/models.py
+++ braincomua_project/parser_app/models.py
@@ -1,3 +1,24 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-# Create your models here.
+
+class Status(models.TextChoices):
+    NEW = "NE", "New"
+    DONE = "DO", "Done"
+    FAILED = "FA", "Failed"
+
+
+class ProductInfo(models.Model):
+    link = models.URLField(unique=True)
+    name = models.CharField(max_length=255, null=True, blank=True)
+    color = models.CharField(max_length=100, null=True, blank=True)
+    builtin_memory = models.CharField(max_length=20, null=True, blank=True)
+    manufacturer = models.CharField(max_length=100, null=True, blank=True)
+    price_regular = models.CharField(max_length=11, null=True, blank=True)
+    price_sale = models.CharField(max_length=11, null=True, blank=True)
+    sku = models.CharField(max_length=50, null=True, blank=True)
+    reviews_count = models.IntegerField(null=True, blank=True)
+    images = ArrayField(models.CharField(max_length=255), null=True, blank=True)
+    characteristics = models.JSONField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
