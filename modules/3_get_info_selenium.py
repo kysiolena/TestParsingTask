ba240babd92e2fb5df63309b49583f5161dd255f
Olena Kysilova <e.a.kiseljova@gmail.com>
2026-02-12 12:13:10 +0200

    Refactoring Xpath in Selenium task

--- modules/3_get_info_selenium.py
+++ modules/3_get_info_selenium.py
@@ -36,7 +36,10 @@ def enter_search_query(wait) -> None:
     # Locating the search input field
     search_input = wait.until(
         EC.presence_of_element_located(
-            (By.XPATH, "//*[@class='header-bottom-in']//input[@type='search']")
+            (
+                By.XPATH,
+                "//*[contains(@class, 'header-bottom-in')]//input[@type='search']",
+            )
         )
     )
     search_input.clear()
@@ -52,7 +55,7 @@ def click_search_button(wait) -> None:
         EC.element_to_be_clickable(
             (
                 By.XPATH,
-                "//input[@class='qsr-submit'][@type='submit']",
+                "//input[contains(@class, 'qsr-submit')][@type='submit']",
             )
         )
     )
